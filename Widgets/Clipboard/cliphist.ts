4290d62f48e7e817f298d31b7a2bb5425324ef91
ARKye03 <rafa2003sanchez@gmail.com>
2024-05-27 22:14:33 -0400

    [not used] add graphical window for cliphist

--- Widgets/Clipboard/cliphist.ts
+++ Widgets/Clipboard/cliphist.ts
@@ -1,40 +1,68 @@
 import { icons } from "assets/Assets";
 import { execAsync } from "resource:///com/github/Aylur/ags/utils.js";
 
-const PopulateClipboard = () => {
-  return execAsync("cliphist list").then((output) => {
-    // Split the output by lines and create a Widget.Label for each line
-    return output.split("\n").map((line) => Widget.Label({ label: line }));
-  });
-};
+//It works, but it's kinda meh the way cliphist works, so it's not worth it
 
-export const ClipboardManager = () =>
-  Widget.Window({
+const WINDOW_NAME = "cliphist";
+const clips = async () => {
+  try {
+    const clipList = await Utils.execAsync(
+      'bash -c "cliphist list | awk \'{\\$1=\\"\\"; print}\'"'
+    );
+    const items = clipList.split("\n");
+    console.log(items);
+    return items;
+  } catch (err) {
+    console.error(err);
+    return [];
+  }
+};
+export const ClipboardManager = async () => {
+  const clipItems = await clips();
+  const clipButtons = clipItems.map((item) =>
+    Widget.Button({
+      child: Widget.Label(item),
+      onClicked: () => {
+        execAsync(`wl-copy ${item}`);
+        App.closeWindow(WINDOW_NAME);
+      },
+    })
+  );
+  return Widget.Window({
     name: "cliphist",
     class_name: "cliphist",
     exclusivity: "normal",
     visible: false,
-    keymode: "on-demand",
+    keymode: "exclusive",
     child: Widget.Box({
       class_name: "cliphist_master_box",
       vertical: true,
+      spacing: 10,
       children: [
         Widget.Box({
           class_name: "clip_entry_box",
+          spacing: 10,
           children: [
             Widget.Icon({
               size: 25,
               icon: icons.Clipboard,
             }),
-            Widget.Entry({}),
+            Widget.Entry({
+              hexpand: true,
+            }),
           ],
         }),
         Widget.Scrollable({
+          class_name: "clip_scroll_box",
+          hscroll: "never",
+          vscroll: "automatic",
           child: Widget.Box({
             vertical: true,
-            children: [],
+            spacing: 10,
+            children: clipButtons,
           }),
         }),
       ],
     }),
   });
+};

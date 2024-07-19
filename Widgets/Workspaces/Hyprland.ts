5959f5305ebfddc42e00e8e83600b7f7960df8a2
ARKye03 <rafa2003sanchez@gmail.com>
2024-07-19 11:41:01 -0400

    fixed some styles, activates Hyprland Mode, formatted Hyprland.ts via Prettier

--- Widgets/Workspaces/Hyprland.ts
+++ Widgets/Workspaces/Hyprland.ts
@@ -2,40 +2,60 @@ import Hyprland from "resource:///com/github/Aylur/ags/service/hyprland.js";
 import Widget from "resource:///com/github/Aylur/ags/widget.js";
 import { execAsync } from "resource:///com/github/Aylur/ags/utils.js";
 
-export default () => Widget.Box({
+export default () =>
+  Widget.Box({
     className: "workspaces",
     setup: (self) => {
-        const Wicons = ["", " ", " ", "󰨞 ", " ", " ", "󰭹 ", " ", " ", "󰊖 ", " "];
-        const workspacesCount = 10;
-        let workspaceStates = Array.from({ length: workspacesCount }, (_, i) => ({
-            id: i + 1,
-            focused: false,
-            hasWindows: false
+      const Wicons = [
+        "",
+        " ",
+        " ",
+        "󰨞 ",
+        " ",
+        " ",
+        "󰭹 ",
+        " ",
+        " ",
+        "󰊖 ",
+        " ",
+      ];
+      const workspacesCount = 10;
+      let workspaceStates = Array.from({ length: workspacesCount }, (_, i) => ({
+        id: i + 1,
+        focused: false,
+        hasWindows: false,
+      }));
+
+      const updateWorkspaceStates = () => {
+        workspaceStates = workspaceStates.map((ws) => ({
+          ...ws,
+          focused: Hyprland.active.workspace.id === ws.id,
+          hasWindows: Hyprland.workspaces.some(
+            (workspace) => workspace.id === ws.id && workspace.windows > 0
+          ),
         }));
+      };
 
-        const updateWorkspaceStates = () => {
-            workspaceStates = workspaceStates.map(ws => ({
-                ...ws,
-                focused: Hyprland.active.workspace.id === ws.id,
-                hasWindows: Hyprland.workspaces.some(workspace => workspace.id === ws.id && workspace.windows > 0),
-            }));
-        };
+      const createWorkspaceButton = (ws: {
+        focused: any;
+        hasWindows: any;
+        id: string | number;
+      }) => {
+        const className = ws.focused ? "focused" : ws.hasWindows ? "work" : "";
+        return Widget.Button({
+          onClicked: () =>
+            execAsync(`/usr/bin/hyprctl dispatch workspace ${ws.id}`),
+          child: Widget.Label(`${Wicons[ws.id]}`),
+          className,
+        });
+      };
 
-        const createWorkspaceButton = (ws: { focused: any; hasWindows: any; id: string | number; }) => {
-            const className = ws.focused ? "focused" : ws.hasWindows ? "work" : "";
-            return Widget.Button({
-                onClicked: () => execAsync(`/usr/bin/hyprctl dispatch workspace ${ws.id}`),
-                child: Widget.Label(`${Wicons[ws.id]}`),
-                className,
-            });
-        };
+      const updateWorkspaces = () => {
+        updateWorkspaceStates();
+        self.children = workspaceStates.map(createWorkspaceButton);
+      };
 
-        const updateWorkspaces = () => {
-            updateWorkspaceStates();
-            self.children = workspaceStates.map(createWorkspaceButton);
-        };
-
-        self.hook(Hyprland.active.workspace, updateWorkspaces, "changed");
-        updateWorkspaces();
+      self.hook(Hyprland.active.workspace, updateWorkspaces, "changed");
+      updateWorkspaces();
     },
-});
+  });
--- Widgets/Workspaces/Switch.ts
+++ Widgets/Workspaces/Switch.ts
@@ -3,8 +3,8 @@ import GLib from "types/@girs/glib-2.0/glib-2.0";
 const DESKTOP_SESSION = GLib.getenv("DESKTOP_SESSION")
 let Workspaces;
 if (DESKTOP_SESSION === "hyprland") {
-    const HyprlandWorkspaces = (await import("./Hyprland"))
-    Workspaces = HyprlandWorkspaces.default
+ const HyprlandWorkspaces = (await import("./Hyprland"))
+ Workspaces = HyprlandWorkspaces.default
 }
 else if (DESKTOP_SESSION === "river") {
     const RiverWorkspaces = (await import("./River"))

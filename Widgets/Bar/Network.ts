4ec7efde2c1da8083635fb80d74eb0295ad66084
ARKye03 <rafa2003sanchez@gmail.com>
2024-07-20 18:39:43 -0400

    Formatted

--- Widgets/Bar/Network.ts
+++ Widgets/Bar/Network.ts
@@ -1,27 +1,29 @@
-
 const network = await Service.import("network");
 
-const WifiIndicator = () => Widget.Box({
-    spacing: 10,
-    class_name: "wifi_indicator",
-},
+const WifiIndicator = () =>
+  Widget.Box(
+    {
+      spacing: 10,
+      class_name: "wifi_indicator",
+    },
     Widget.Icon({
-        icon: network.wifi.bind('icon_name'),
+      icon: network.wifi.bind("icon_name"),
     }),
     Widget.Label({
-        label: network.wifi.bind('ssid')
-            .as(ssid => ssid || 'Unknown'),
-    }),
-)
-const WiredIndicator = () => Widget.Icon({
+      label: network.wifi.bind("ssid").as((ssid) => ssid || "Unknown"),
+    })
+  );
+const WiredIndicator = () =>
+  Widget.Icon({
     class_name: "clock",
-    icon: network.wired.bind('icon_name'),
-})
+    icon: network.wired.bind("icon_name"),
+  });
 
-export const NetworkIndicator = () => Widget.Stack({
+export const NetworkIndicator = () =>
+  Widget.Stack({
     children: {
-        wifi: WifiIndicator(),
-        wired: WiredIndicator(),
+      wifi: WifiIndicator(),
+      wired: WiredIndicator(),
     },
-    shown: network.bind('primary').as(p => p || 'wifi'),
-})
+    shown: network.bind("primary").as((p) => p || "wifi"),
+  });

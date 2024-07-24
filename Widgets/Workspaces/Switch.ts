2f5edf3d6fdf0277e0caf85527a610f1fb4457a5
ARKye03 <rafa2003sanchez@gmail.com>
2024-07-24 11:27:32 -0400

    Extracet DESKTOP_SESSION to globals

--- Widgets/Workspaces/Switch.ts
+++ Widgets/Workspaces/Switch.ts
@@ -1,16 +1,13 @@
-import GLib from "types/@girs/glib-2.0/glib-2.0";
+import { Globals } from "Widgets/userVars";
 
-const DESKTOP_SESSION = GLib.getenv("DESKTOP_SESSION")
 let Workspaces;
-if (DESKTOP_SESSION === "hyprland") {
- const HyprlandWorkspaces = (await import("./Hyprland"))
- Workspaces = HyprlandWorkspaces.default
+if (Globals.DESKTOP_SESSION === "hyprland") {
+  const HyprlandWorkspaces = await import("./Hyprland");
+  Workspaces = HyprlandWorkspaces.default;
+} else if (Globals.DESKTOP_SESSION === "river") {
+  const RiverWorkspaces = await import("./River");
+  Workspaces = RiverWorkspaces.default;
+} else {
+  console.error("<----F---->");
 }
-else if (DESKTOP_SESSION === "river") {
-    const RiverWorkspaces = (await import("./River"))
-    Workspaces = RiverWorkspaces.default
-}
-else {
-    console.error("<----F---->")
-}
-export default Workspaces
+export default Workspaces;

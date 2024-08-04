f1fe588374ece1f86d4a71d9960b5a454d192137
ARKye03 <rafa2003sanchez@gmail.com>
2024-08-04 17:28:17 -0400

    [WIP] buttons

--- Modules/Dock/dock.ts
+++ Modules/Dock/dock.ts
@@ -1,11 +1,13 @@
 import { icons } from "assets/Assets";
 import Gio from "gi://Gio";
 import { Globals } from "Modules/userVars";
-const a = Gio.DesktopAppInfo.new_from_filename(
+import { exec, execAsync } from "resource:///com/github/Aylur/ags/utils.js";
+const BraveBrowser = Gio.DesktopAppInfo.new_from_filename(
   `${Globals.HOME}/.local/share/applications/brave-browser.desktop`
 );
-console.log(a);
-console.log(a.list_actions());
+
+const VSCode = Gio.DesktopAppInfo.new("code.desktop");
+
 const rev = Widget.Revealer(
   {
     reveal_child: false,
@@ -15,16 +17,28 @@ const rev = Widget.Revealer(
   Widget.Box(
     { class_name: "dock_box_apps", spacing: 10 },
     Widget.Button({
-      image: Widget.Icon({ icon: icons.AppLauncher, size: 45 }),
+      image: Widget.Icon({
+        icon: BraveBrowser.get_icon()?.to_string() ?? "",
+        size: 45,
+      }),
       vpack: "center",
       hpack: "center",
-      on_clicked: () => a.launch_action("new-window", null),
+      on_clicked: () => BraveBrowser.launch_action("new-window", null),
     }),
     Widget.Button({
-      image: Widget.Icon({ icon: icons.ArchLogo, size: 45 }),
+      image: Widget.Icon({
+        icon: VSCode.get_icon()?.to_string() ?? "",
+        size: 45,
+      }),
       vpack: "center",
       hpack: "center",
+      on_clicked: () => {
+        if (exec("pgrep -x code").length > 0)
+          execAsync("hyprctl dispatch focuswindow code");
+        else VSCode.launch_action(VSCode.list_actions()[0], null);
+      },
     }),
+
     Widget.Button({
       image: Widget.Icon({ icon: icons.PowerButton, size: 45 }),
       vpack: "center",

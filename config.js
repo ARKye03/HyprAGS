66e7b59a23494aed7873a28c7112dca5965b57be
ARKye03 <rafa03-dev@proton.me>
2024-08-26 14:33:50 -0400

    Absolute path for bun

--- config.js
+++ config.js
@@ -2,20 +2,20 @@ const entry = App.configDir + "/main.ts";
 const outdir = "/tmp/ags/js";
 
 try {
-  await Utils.execAsync([
-    "bun",
-    "build",
-    entry,
-    "--outdir",
-    outdir,
-    "--external",
-    "resource://*",
-    "--external",
-    "gi://*",
-  ]);
-  await import(`file://${outdir}/main.js`);
+	await Utils.execAsync([
+		"/home/archkye/.bun/bin/bun",
+		"build",
+		entry,
+		"--outdir",
+		outdir,
+		"--external",
+		"resource://*",
+		"--external",
+		"gi://*",
+	]);
+	await import(`file://${outdir}/main.js`);
 } catch (error) {
-  console.error(error);
+	console.error(error);
 }
 
 export {};
